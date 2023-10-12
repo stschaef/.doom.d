ac1ebd3a0c0efbd8d13e1ac2adfa488cf593cd6a
Steven Schaefer <stschaef@umich.edu>
2023-10-11 20:22:28 -0400

    init commit

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/Users/stevenschaefer/org/journal/09-09-2-23.org" "/Users/stevenschaefer/org/journal/10-11-2023.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'pyenv-mode-set 'disabled t)
